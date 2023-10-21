3612869d416ac94dd64b8cccaa79dd4cafeb25d5
ZaibLComrade <kmjahanzaib@gmail.com>
2023-10-21 16:54:15 +0600

    Fixed not found page responsivness bug

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,9 +1,11 @@
 export default function NotFound() {
 	return <div className="absolute text-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-		<h1 className="text-[300px]">404</h1>
-		<div className="space-y-4">
-			<p className="text-5xl">It looks like you're lost</p>
-			<p className="text-lg">The page you're looking for doesn't exist</p>
+		<div className="max-w-[700px]">
+			<h1 className="md:text-[300px] text-8xl">404</h1>
+			<div className="space-y-4">
+				<p className="text-2xl md:text-5xl">It looks like you're lost</p>
+				<p className="text-lg">The page you're looking for doesn't exist</p>
+			</div>
 		</div>
 	</div>
 }
